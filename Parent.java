8b63e292d2a77c04f5b36f4b00a6d182763954df
Selina <szou1@stuy.edu>
2018-01-10 08:11:11 -0500

    add Parent class

---
+++ Parent.java
@@ -0,0 +1,24 @@
+public class Parent implements Login{
+
+    //login info
+    private String _email;
+    private String _password;
+	
+    //Child name related info
+    private String _childFirstName;
+    private String _childMiddleInitial;
+    private String _childLastName;
+
+    //Child ID
+    private int _childStudentID;
+	
+    //Parent name info
+    private String _firstName;
+    private String _middleInitial;
+    private String _lastName;
+    
+    public Parent() {
+	
+    }
+    
+}//end class
