db38708758733f8cbc4e607462b325e0f8938da1
Rubin Peci <rubinpeci@yahoo.com>
2018-01-10 23:10:54 -0500

    Complete test of file input

--- input_test/FileTest.java
+++ input_test/FileTest.java
@@ -1,20 +1,25 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.File;
+import java.io.*;
 
 public class FileTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		String line = null;
-	
-		File f = new File("test.txt");
-		FileReader fr = new FileReader(f); //file reader object takes parameter as location of the plaintext file in the constructor
-		BufferedReader test = new BufferedReader(fr); //is a wrapper class for the FileReader that allows more functionality
+		String[] lines = new String[	
+
+		try {
+			FileReader fr = new FileReader("test.txt"); //file reader object takes parameter as location of the plaintext file in the constructor
+			BufferedReader test = new BufferedReader(fr); //is a wrapper class for the FileReader that allows more functionality
+			
+			while ((line = test.readLine()) != null) {
+				System.out.println(line);
+			}
+
+		} catch (FileNotFoundException e) {
+			System.out.println("u dun goofed");
+		}
 
 
-		while((line = test.readLine()) != null) {	
-			System.out.println(line);
-		}						
+							
 	}
 }
 
