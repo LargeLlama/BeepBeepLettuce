7d075897f472957ec6b0c2c735b695f8b573979c
Selina Zou <szou1@stuy.edu>
2018-01-16 00:59:51 -0500

    finish Sorts

--- Sorts.java
+++ Sorts.java
@@ -1,40 +1,59 @@
-import java.util.ArrayList;
-
 public class Sorts{
-        public static void mergeSort( double[] data ) 
-    {
-      	//number of req passes as discussed in class
-	int totalPasses = data.size() - 1;
-	//initialize counter at 0
-	int currentPasses = 0;
+    
+    //Sorts implements quickSort algorithm
+    //  recursively partioning array
+    //  into values less than and greater than pivot point
 
-	//for storage of index
-	int swapperIndex;
+    //helper method
+    public static int part( double[] arr, int left, int right){
+	//uses last element as pivot value
+	//  and then places arr values
+	//  to the right of pivot val
+	//  if <= said val
 
-	while(currentPasses < totalPasses){
-	    //initiate swapperIndex as currentPasses index
-	    // not changed if currentPasses is smaller or equal to
-	    // everything to its right
-	    swapperIndex = currentPasses;
-
-	    //iterate thru array
-	    for(int i = currentPasses; i < data.length; i++){
-		//if any values at indexes after currentPasses are smaller 
-		if (data[i].compareTo(data[swapperIndex]) < 0)
-		    //store index value in swapperIndex
-		    swapperIndex = i;
-	    }
-	    
-	    //insert data at swapperIndex at currentPasses index
-	    data.add(currentPasses, data[swapperIndex]);
-
-	    //remove data at swapperIndex
-	    data.remove(swapperIndex + 1);
-	    
-	    //increase currentPasses count
-	    currentPasses++;
-	}
-
+	//tracks index of smallest val
+        int i = (left-1);
+	//pivot val
+	double pivot = arr[right];
 	
-    }//end selectionSortV
-}
+	for (int x = left; x < right; x++){
+	    //if <=
+	    if (arr[x] <= pivot){
+		i++;
+		//swap arr[i] and arr[x]
+		double temp = arr[x];
+		arr[x] = arr[i];
+		arr[i] = temp;
+	    }
+	}
+ 
+	//swap arr[i+1] and pivot
+	double temp = arr[i+1];
+	arr[i+1] = arr[right];
+	arr[right] = temp;
+ 
+        return i+1;
+    }
+
+    //helper method that specifies
+    //  leftmost and rightmost points of operation
+    public static void helperSort( double[] arr, int left, int right ){
+	if(left < right){
+	    //partition arr
+	    int partition = part(arr, left, right);
+
+	    //recursively sort both sides of partition
+	    helperSort(arr, left, partition - 1);
+            helperSort(arr, partition + 1, right);
+	}
+    }
+
+
+    public static void sort(double[] arr){
+	int left = 0;
+	int right = arr.length - 1;
+	helperSort(arr, left, right);
+    }
+    
+
+}//end class
